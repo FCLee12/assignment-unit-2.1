8ca8766c4b21d662249c281db66ac4cde781c3b1
Fue Lee <fueclee.12@gmail.com>
2023-01-27 18:54:44 -0600

    completed stretch goal question 18, completed assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -106,5 +106,4 @@ switch ( luckyNumber ) {
 };
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
+adventurous ? console.log( 'Adventures are great!' ) : console.log( 'How about we stay home?' );
